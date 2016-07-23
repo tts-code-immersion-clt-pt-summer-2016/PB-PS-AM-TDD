3faf4db8417cbc28c23d401e63686f71cb19cb65
Priyam Sarma <priyam.sarma@gmail.com>
2016-07-23 17:05:01 -0400

    Modify with hash instead of array, add methods, add loop for user to complete all activities for inventory

--- inventory.rb
+++ inventory.rb
@@ -1,20 +1,110 @@
 #INVENTORY OF CANDY 
 
-
-items = [['Kit Kat', 3], ['Reeses', 10], ['M&Ms', 4], ['Hersheys', 13]]
-
-def inventory(array)
-  puts array 
+# Define all Methods
+# MEthod for displaying inventory
+def inventory(items)
+  puts "This is the candy in your inventory:" 	
+  items.each{|key,value| puts "#{key}: #{value}"} 
 end
 
-def add_item(array, item_to_add, quantity)
-  array.push([item_to_add, quantity])
+# method for updating item to existing inventory
+def update_item(items)
+	puts "Which item would you like to update: #{items.keys}?"
+	item = gets.chomp
+	until items.has_key?(item)
+		puts "Please Enter an Item that exists in inventory!"
+		inventory(items)
+		item = gets.chomp
+	end
+	puts "What is the new inventory value for #{item}?"
+	value = gets.chomp.to_i
+	# Enhance by using REGEXP to check for valid integer values
+	items[item] = value
 end  
-  
-def update_inventory(position, new_item)
-end 
+
+# method for updating existing inventory
+def add_item(items)
+	puts "What would you like to add to inventory"
+	item = gets.chomp
+	while items.has_key?(item)
+		puts "Please Enter an Item that doesn't already exist, otherwise use the Update Inventory option!"
+		inventory(items)
+		item = gets.chomp
+	end	
+	puts "What is the new inventory value for #{item}"
+	# Enhance by using REGEXP to check for valid integer values
+	value = gets.chomp.to_i
+	items[item] = value
+end   
+
+# method for deleting existing inventory  
+def delete_item(items)
+	puts "What would you like to delete from inventory"
+	item = gets.chomp
+	until items.has_key?(item)
+		puts "Please Enter an Item to delete that exists in inventory"
+		inventory(items)
+		item = gets.chomp
+	end	
+	# Enhance by checking for non 0 inventory values and warning user there is still inventory
+	items.delete(item)
+ end   
+
+# Method for Printing Options
+def options()
+	puts "Please Select from the following options:
+	0: Enter \"0\" to Exit inventory application
+	1: Enter \"1\" to View inventory
+	2: Enter \"2\" to Update an Item in inventory
+	3: Enter \"3\" to Add an Item to inventory
+	4: Enter \"4\" to Delete an Item from inventory
+	"
+end
+# Creating a hash of inital inventory of candy
+# Hashes have some advantages over arrays, in not needing to define our own methods
+items = {'Kit Kat' => 3, 'Reeses' => 10, 'M&Ms' => 4, 'Hershey' => 13}
+
+exit = false
+
+until exit
+	options()
+	user_action = gets.chomp
+	until user_action == "0"
+
+		if user_action == "1"
+			inventory(items)
+			puts "\n"
+			options()
+			user_action = gets.chomp
+		elsif user_action == "2"
+			inventory(items)
+			puts "\n"
+			update_item(items)
+			puts "\n"
+			options()
+			user_action = gets.chomp
+		elsif user_action == "3"
+			inventory(items)
+			puts "\n"
+			add_item(items)
+			puts "\n"
+			options()
+			user_action = gets.chomp			
+		elsif user_action == "4"
+			inventory(items)
+			puts "\n"			
+			delete_item(items)
+			puts "\n"
+			options()
+			user_action = gets.chomp			
+		end
+	end
+	puts "Exiting inventory application"
+	exit = true
+end
 
 
-inventory(items)
-add_item(items, 'Twix', 10)
-inventory(items)
+# inventory(items)
+# add_item(items)
+# add_item(items)
+# inventory(items)
